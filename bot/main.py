0a6c03cea8e69a444c7fd7d4138946a48dbaa265
ega666881 <63003334+ega666881@users.noreply.github.com>
2024-11-17 19:25:04 +0300

    1

--- bot/main.py
+++ bot/main.py
@@ -30,6 +30,7 @@ async def startup():
 async def check_subscription(request: MessageRequest):
     try:
         user_channel_status = await bot.get_chat_member(chat_id=request.channelId, user_id = request.tgId)
+        print(user_channel_status)
         if user_channel_status != 'left':
             print('true')
             return {"status": "success", "check": True}
